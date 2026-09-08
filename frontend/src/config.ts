9d655752e2812da1248800b7283ba1591c3fa0ce
abiroyCoder <abiroykarmakar543@gmail.com>
2026-09-08 19:51:25 +0530

    Update frontend dependencies

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -26,3 +26,4 @@ export const setContractAddress = (address: string) => {
 // Dev checkpoint: 2026-09-07T22:41:08+05:30
 // Dev checkpoint: 2026-09-08T05:44:34+05:30
 // Dev checkpoint: 2026-09-08T12:47:59+05:30
+// Dev checkpoint: 2026-09-08T19:51:25+05:30
