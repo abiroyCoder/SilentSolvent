3466fed5292a8f388941145d7c01e0f1d19f402a
abiroyCoder <abiroykarmakar543@gmail.com>
2026-09-09 09:58:17 +0530

    Add progress bar styling

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -28,3 +28,4 @@ export const setContractAddress = (address: string) => {
 // Dev checkpoint: 2026-09-08T12:47:59+05:30
 // Dev checkpoint: 2026-09-08T19:51:25+05:30
 // Dev checkpoint: 2026-09-09T02:54:51+05:30
+// Dev checkpoint: 2026-09-09T09:58:17+05:30
