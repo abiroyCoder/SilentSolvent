ff28b9c50e74e4dccdcb1899bd892c25fcfcb3dd
abiroyCoder <abiroykarmakar543@gmail.com>
2026-09-09 17:01:42 +0530

    Implement useContractState hook

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -29,3 +29,4 @@ export const setContractAddress = (address: string) => {
 // Dev checkpoint: 2026-09-08T19:51:25+05:30
 // Dev checkpoint: 2026-09-09T02:54:51+05:30
 // Dev checkpoint: 2026-09-09T09:58:17+05:30
+// Dev checkpoint: 2026-09-09T17:01:42+05:30
