3ef391bd776003f0718e055fdbc76552460980ec
abiroyCoder <abiroykarmakar543@gmail.com>
2026-09-10 07:08:34 +0530

    Fix unhandled promise rejection in submitTx

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -31,3 +31,4 @@ export const setContractAddress = (address: string) => {
 // Dev checkpoint: 2026-09-09T09:58:17+05:30
 // Dev checkpoint: 2026-09-09T17:01:42+05:30
 // Dev checkpoint: 2026-09-10T00:07:08+05:30
+// Dev checkpoint: 2026-09-10T07:08:34+05:30
