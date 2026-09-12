596ec9bc834a13890f84b80d1f58d84ad58a177e
abiroyCoder <abiroykarmakar543@gmail.com>
2026-09-12 08:32:34 +0530

    Update package-lock.json

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -38,3 +38,4 @@ export const setContractAddress = (address: string) => {
 // Dev checkpoint: 2026-09-11T11:22:17+05:30
 // Dev checkpoint: 2026-09-11T18:27:42+05:30
 // Dev checkpoint: 2026-09-12T01:29:08+05:30
+// Dev checkpoint: 2026-09-12T08:32:34+05:30
